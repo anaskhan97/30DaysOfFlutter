f0260b8054d74897e50c887aaa29bc0f852b8788
Anas Khan <anas.khan8887@gmail.com>
2024-02-03 09:05:09 +0500

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Codepur";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      drawer: Drawer(),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+    );
+  }
+}
